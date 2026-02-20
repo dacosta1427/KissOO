5bc208e54827a17634509d12a544250304940baa
dacosta <dcd@dcgconsultancy.nl>
2026-02-20 16:10:36 +0100

    Refactor to use Lombok
    - Added lombok.jar to libs/
    - PerstConfig.java: Added @Getter, removed manual getters
    - Actor.java: Added @Getter @Setter, removed manual getters/setters
    
    Reduces boilerplate code significantly

--- src/main/backend/gfe/Actor.java
+++ src/main/backend/gfe/Actor.java
@@ -1,33 +1,26 @@
 package gfe;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.garret.perst.continuous.CVersion;
 
 /**
  * Actor - Represents an entity that can own and perform actions on OOVs.
- * This is a sample domain class to demonstrate Perst integration.
  */
+@Getter
+@Setter
 public class Actor extends CVersion {
     
     private String uuid;
     private String name;
     private String type;  // EMPLOYEE, CORPORATE, RETAIL
-    private boolean active;
+    private boolean active = true;
     private long createdDate;
     
-    /**
-     * Default constructor for Perst
-     */
     public Actor() {
         this.createdDate = System.currentTimeMillis();
-        this.active = true;
     }
     
-    /**
-     * Create a new Actor
-     * 
-     * @param name The actor's name
-     * @param type The actor type
-     */
     public Actor(String name, String type) {
         this();
         this.name = name;
@@ -35,51 +28,6 @@ public class Actor extends CVersion {
         this.uuid = java.util.UUID.randomUUID().toString();
     }
     
-    // Getters and Setters
-    
-    public String getUuid() {
-        return uuid;
-    }
-    
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-        modify();
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-        modify();
-    }
-    
-    public String getType() {
-        return type;
-    }
-    
-    public void setType(String type) {
-        this.type = type;
-        modify();
-    }
-    
-    public boolean isActive() {
-        return active;
-    }
-    
-    public void setActive(boolean active) {
-        this.active = active;
-        modify();
-    }
-    
-    public long getCreatedDate() {
-        return createdDate;
-    }
-    
-    /**
-     * Convert to JSON for API response
-     */
     public java.util.Map<String, Object> toJSON() {
         java.util.Map<String, Object> json = new java.util.HashMap<>();
         json.put("uuid", uuid);
--- src/main/precompiled/nl/dcg/gfe/PerstConfig.java
+++ src/main/precompiled/nl/dcg/gfe/PerstConfig.java
@@ -1,11 +1,15 @@
 package nl.dcg.gfe;
 
+import lombok.Getter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
- * GFE Perst Integration - PerstConfig
- * 
  * Configuration class for Perst integration.
  * Reads settings from application.ini
  */
+@Getter
 public class PerstConfig {
     private static final String CONFIG_FILE = "application.ini";
     private static PerstConfig instance;
@@ -26,35 +30,21 @@ public class PerstConfig {
     }
     
     private void loadConfig() {
-        java.util.Properties props = new java.util.Properties();
-        try (java.io.FileInputStream fis = new java.io.FileInputStream(CONFIG_FILE)) {
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream(CONFIG_FILE)) {
             props.load(fis);
             
-            String enabled = props.getProperty("PerstEnabled", "false");
-            this.perstEnabled = "true".equalsIgnoreCase(enabled);
-            
+            this.perstEnabled = "true".equalsIgnoreCase(props.getProperty("PerstEnabled", "false"));
             this.databasePath = props.getProperty("PerstDatabasePath", "oodb");
-            
-            String poolSize = props.getProperty("PerstPagePoolSize", "536870912");
-            this.pagePoolSize = Integer.parseInt(poolSize);
+            this.pagePoolSize = Integer.parseInt(props.getProperty("PerstPagePoolSize", "536870912"));
             
             System.out.println("[PerstConfig] Perst Enabled: " + perstEnabled);
             System.out.println("[PerstConfig] Database Path: " + databasePath);
             
-        } catch (java.io.IOException e) {
+        } catch (IOException e) {
             System.out.println("[PerstConfig] Config file not found, using defaults. Perst disabled.");
+        } catch (NumberFormatException e) {
+            System.out.println("[PerstConfig] Invalid page pool size, using default: " + e.getMessage());
         }
-    }
-    
-    public boolean isPerstEnabled() {
-        return perstEnabled;
-    }
-    
-    public String getDatabasePath() {
-        return databasePath;
-    }
-    
-    public int getPagePoolSize() {
-        return pagePoolSize;
     }
 }
