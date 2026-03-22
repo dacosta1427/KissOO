6263662f46f7af894ec1a3b01c488556c78986e9
dacosta <dcg@dcgconsultancy.nl>
2026-03-22 21:34:13 +0100

    chore: Add .svelte-kit to .gitignore
    - SvelteKit generated files should not be tracked
    - Removes generated build artifacts from version control

--- src/main/frontend-svelte/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/login/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.MHRk7XCs.js","_app/immutable/chunks/B2QSKqcC.js","_app/immutable/chunks/Bi3IMxzF.js","_app/immutable/chunks/Cj5Ku0qB.js","_app/immutable/chunks/CpEWiglc.js","_app/immutable/chunks/DCidFKtF.js"];
-export const stylesheets = [];
-export const fonts = [];
