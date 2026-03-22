5d98d34fd088a95f1f2afaf420ba2cec4af28596
dacosta <dcg@dcgconsultancy.nl>
2026-03-22 12:02:23 +0100

    Fix tsconfig: use kit.alias instead of paths, remove baseUrl

--- src/main/frontend-svelte/svelte.config.js
+++ src/main/frontend-svelte/svelte.config.js
@@ -1,9 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$lib: resolve('./src/lib'),
+			$components: resolve('./src/components'),
+			$services: resolve('./src/services'),
+			$stores: resolve('./src/stores')
+		}
 	}
 };
 
