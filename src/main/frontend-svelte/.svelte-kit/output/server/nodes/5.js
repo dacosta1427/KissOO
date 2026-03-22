5310ebad65cee58db3474d42b8974db4b57d104a
dacosta <dcg@dcgconsultancy.nl>
2026-03-22 14:35:48 +0100

    Remove .svelte-kit

--- src/main/frontend-svelte/.svelte-kit/output/server/nodes/5.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 5;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/users/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/5.DnXPLfDD.js","_app/immutable/chunks/B2QSKqcC.js","_app/immutable/chunks/Bi3IMxzF.js","_app/immutable/chunks/CpEWiglc.js"];
-export const stylesheets = [];
-export const fonts = [];
