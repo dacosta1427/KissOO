5e5ab9f19ee86e2e52744f672ca0f4c452f2de74
dacosta <dcg@dcgconsultancy.nl>
2026-03-22 11:55:19 +0100

    Remove .svelte-kit from git (should be generated locally)

--- src/main/frontend-svelte/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.Boul6fnz.js","_app/immutable/chunks/B2QSKqcC.js","_app/immutable/chunks/BgE6Kk8l.js","_app/immutable/chunks/C5QRPDXU.js","_app/immutable/chunks/DlBzjGQM.js"];
-export const stylesheets = [];
-export const fonts = [];
